7eef0c3abc3367d83116203243b816231efe9fc2
Stephen Wong <4060163+yellowcandle@users.noreply.github.com>
2023-06-01 12:22:26 +0800

    Read and display the temperature data from the API Feed.

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-import json
-import folium
 import requests
 
-with open('HKONWS.json') as f:
-    data = json.load(f)
+# Get the temperature data from the API
+url = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en"
+response = requests.get(url)
+weather_data = response.json()
 
-m = folium.Map(location=[22.3193, 114.1694], zoom_start=11)
+# Extract the temperature data of all stations
+stations = weather_data["temperature"]["data"]
 
-for feature in data['features']:
-    if feature['properties']['TypesofWeatherStation_en'] == 'AUTOMATIC WEATHER STATION':
-        latitude = feature['properties']['Latitude']
-        longitude = feature['properties']['Longitude']
-        temperature_data = data['temperature']['data']
-        for temp in temperature_data:
-            folium.Marker(location=[latitude, longitude],
-                          popup=f"The current temperature in {temp['place']} is {temp['value']} degrees Celsius.").add_to(
-                m)
+# Display the temperature data of all stations
+for station in stations:
+    print(f"Station: {station['place']}, Temperature: {station['value']}°C")
